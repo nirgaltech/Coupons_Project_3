069602816b045262d272ddd736113bc6adbaa3b5
Nir <nirg@johnbryce.co.il>
2024-01-10 14:36:28 +0200

    Added comment in main and created a Company Spring Bean

--- src/main/java/com/example/spring_project/SpringProjectApplication.java
+++ src/main/java/com/example/spring_project/SpringProjectApplication.java
@@ -1,6 +1,7 @@
 package com.example.spring_project;
 
 
+import com.example.spring_project.beans.Company;
 import com.example.spring_project.facade.ClientFacade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +16,10 @@ public class SpringProjectApplication {
 
 	public static void main(String[] args) {
 
+		// run the Spring Application context!
 		ConfigurableApplicationContext ctx=SpringApplication.run(SpringProjectApplication.class, args);
+		Company comp = ctx.getBean(Company.class);
+
 	}
 
 	@Bean
